7f54ccab360ef5bf83d5f2f07cc18a930452eeec
tian_1800 <tian.sudibyo@gmail.com>
2026-05-20 00:37:45 +0800

    Allow sentry script

--- apps/web/instrumentation-client.ts
+++ apps/web/instrumentation-client.ts
@@ -3,6 +3,12 @@ import * as Sentry from '@sentry/nextjs';
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   environment: process.env.SENTRY_ENVIRONMENT,
+  enableLogs: true,
+  integrations: [
+    Sentry.consoleLoggingIntegration({
+      levels: ['error'],
+    }),
+  ],
 });
 
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
